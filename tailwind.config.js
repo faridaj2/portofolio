b0faa4457a502b1672e9017157d5443fcdaf8e6d
faridaj2 <ajfarid6@gmail.com>
2023-10-01 09:42:55 +0700

    firt commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#61C9AE',
+                secondary: '#1C332D',
+                secondaryDark: '#121212'
+            }
+        }
+    }
+}
